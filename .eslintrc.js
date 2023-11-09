aeeab7e26eca63108239fb004f7e358f24b9763e
kvlinden <kvlinden@calvin.edu>
2023-11-09 13:43:53 -0500

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    root: true,
+    extends: ["universe/native"],
+    rules: {
+        "no-undef": "off",
+        "indent": ["error", 4],
+        "prettier/prettier": "off",
+    },
+};
--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
 /**
  * This application presents a simple list of players from the example monopoly
@@ -7,28 +7,32 @@ import { ActivityIndicator, FlatList, Text, View } from 'react-native';
  * - https://reactnative.dev/docs/network>
  */
 export default App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch('https://cs262-monopoly-service.herokuapp.com/players/')
-        .then((response) => response.json())
-        .then((json) => setData(json))
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-  }, []);
+    useEffect(() => {
+        fetch("https://cs262-monopoly-service.herokuapp.com/players/")
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
 
-  return (
-      <View style={{ flex: 1, padding: 24 }}>
-        {isLoading ? <ActivityIndicator/> : (
-            <FlatList
-                data={data}
-                keyExtractor={({ id }, index) => id.toString()}
-                renderItem={({ item }) => (
-                    <Text>{item.id}, {item.emailaddress}, {item.name}</Text>
-                )}
-            />
-        )}
-      </View>
-  );
+    return (
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }, index) => id.toString()}
+                    renderItem={({ item }) => (
+                        <Text>
+                            {item.id}, {item.emailaddress}, {item.name}
+                        </Text>
+                    )}
+                />
+            )}
+        </View>
+    );
 };
